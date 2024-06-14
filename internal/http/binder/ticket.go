2fee96685b65679d423f99b4b0ba442119d6c4ed
putiadiva <adiva.puti@gmail.com>
2024-06-14 17:12:18 +0700

    Fix/Ticket : add validation in binfer

--- internal/http/binder/ticket.go
+++ internal/http/binder/ticket.go
@@ -3,9 +3,9 @@ package binder
 import "github.com/Giafn/Depublic/internal/entity"
 
 type TicketCreateRequest struct {
-	IDEvent       string `json:"idEvent"`
-	IDTransaction string `json:"idTransaction"`
-	Data          []entity.Person `json:"data"`
+	IDEvent       string `json:"idEvent" validate:"required"`
+	IDTransaction string `json:"idTransaction" validate:"required"`
+	Data          []entity.Person `json:"data" validate:"required"`
 }
 
 type TicketUpdateRequest struct {
